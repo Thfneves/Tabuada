ae86994847da6da9f421053248b9efadba322251
Thiago NeveS <140116209+Thfneves@users.noreply.github.com>
2023-10-02 18:22:40 -0300

    Updtade
    Atualizei o JS para barrar a consulta na tabuada quando não tiver nenhuma informação accionada, usando IF ELSE e ALERT.
    Atribui também ao JS para nos informar na pagina os números que representam o resultado ex..  10 * 3
    10 * 1=
    10 * 2= 
    10 * 3=

--- Tabuada_script.js
+++ Tabuada_script.js
@@ -1,10 +1,19 @@
-function Tabuada(){
-var num = window.document.getElementById('num')
-var end = document.getElementById('end')
-var n = Number(num.value)
-var e = Number(end.value)
-var r = ''
-var res = window.document.getElementById('resposta')
-r = n*e
-res.innerHTML = `A resposta é: ${r}`
+function Tabuada() {
+  var num = window.document.getElementById("num");
+  var end = document.getElementById("end");
+  if (num.value.length == 0 || end.value.length == 0) {
+    window.alert("[ERRO] Confira os dados!");
+  } else {
+    var n = Number(num.value);
+    var e = Number(end.value);
+    var x = 0;
+    var r = ``;
+    var res = window.document.getElementById("resposta");
+    res.innerHTML = `A  Tabuada do ${n} é :<br>`;
+    do {
+      r = n * x;
+      res.innerHTML += `${n} x ${x} = ${r}<br>`;
+      x++;
+    } while (x <= e);
+  }
 }
